aa5b000147ea29d5fbb06ace1fd6174158abd953
Klepitskaya <jenka1330.gmail.com>
2021-12-27 22:03:07 +0300

    xml configuration

--- src/main/java/com/ita/u1/App.java
+++ src/main/java/com/ita/u1/App.java
@@ -1,6 +1,15 @@
 package com.ita.u1;
 
+import com.ita.u1.service.MovieReport;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        MovieReport movieReport = context.getBean("movieReport", MovieReport.class);
+        movieReport.generate();
+        context.close();
     }
 }
